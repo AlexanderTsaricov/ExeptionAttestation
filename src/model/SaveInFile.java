25975823e349c804c488fda37fb7cb1d6a173a1f
Alexandr Vinokurov <salispiligrim@yandex.ru>
2024-10-24 18:02:09 +0500

    add save class

---
+++ src/model/SaveInFile.java
@@ -0,0 +1,13 @@
+package model;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class SaveInFile {
+    public void save (String saveData, String surname) throws IOException {
+
+        FileWriter fw = new FileWriter(surname + ".txt");
+        fw.write(saveData);
+        fw.write("\n");
+        fw.close();
+    }
+}
